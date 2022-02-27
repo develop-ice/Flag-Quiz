de4a8ce855a6b3db85a430bd25368d6c9f92e5e9
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-02-26 22:49:37 -0500

    Splash Screen

--- app/src/main/java/com/android/flagquiz/MainActivity.java
+++ app/src/main/java/com/android/flagquiz/MainActivity.java
@@ -1,12 +1,15 @@
 package com.android.flagquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Splash Screen
+        setTheme(R.style.AppTheme);
+        // onCreate
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
